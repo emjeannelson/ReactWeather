bf85ad699498d80b7676e6736f06930e5cd32606
Emily Nelson <emilynelson@Emilys-MacBook-Pro.local>
2017-01-18 13:48:31 +0800

    Style about page, add copy, and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,23 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather app built on React. I built this for the <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/">Complete React Web App Developer Course</a> on Udemy. I learned a lot while building it!</p>
+      <p>Here are some of the tools I used:</p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework I used.
+          </li>
+          <li>
+            <a href="https://webpack.github.io/">Webpack</a> - I used this build tool during the development process.
+          </li>
+          <li>
+            <a href="http://foundation.zurb.com/">Foundation</a> - The app was styled using the responsive front-end framework Foundation.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+          </li>
+        </ul>
     </div>
   )
 };
