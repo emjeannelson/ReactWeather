dc795365b86d9adf804653353f677b5db13bbdfd
Emily Nelson <emilynelson@Emilys-MacBook-Pro.local>
2017-01-18 11:19:15 +0800

    Add grid layout styles and examples page copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Taipei'>Taipei, Taiwan</Link>
+        </li>
+        <li>
+          <Link to='/?location=Topeka'>Topeka, KS, USA</Link>
+        </li>
+      </ol>
     </div>
   );
 };
