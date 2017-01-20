b1a07deec9e6f99c59422decd89505ab4eb499ba
Emily Nelson <emilynelson@Emilys-MacBook-Pro.local>
2017-01-20 09:33:48 +0800

    Add copy to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,16 +8,16 @@ var About = (props) => {
       <p>Here are some of the tools I used:</p>
         <ul>
           <li>
-            <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework I used.
+            <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework I used. I learned all about what makes React great and how to use its awesome features, including React Router.
           </li>
           <li>
             <a href="https://webpack.github.io/">Webpack</a> - I used this build tool during the development process.
           </li>
           <li>
-            <a href="http://foundation.zurb.com/">Foundation</a> - The app was styled using the responsive front-end framework Foundation.
+            <a href="http://foundation.zurb.com/">Foundation</a> - The app was styled using the responsive front-end framework Foundation. I also built a custom error modal using Foundation components.
           </li>
           <li>
-            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name. Note that Open Weather Map is very liberal with approximate matching of search strings, so you may need to play around to get the custom error modal to appear. This is a problem with the API!
           </li>
         </ul>
     </div>
